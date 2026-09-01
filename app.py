df68061c1ba7db6328bb48cb8bda4462efebf9fa
juliaaguiar1 <jujuaguoiarmmoises@gmail.com>
2026-09-01 20:22:16 -0300

    feat: personaliza mensagem da rota principal

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Checkpoint CI/CD funcionando com sucesso!"
+    return "Checkpoint CI/CD desenvolvido pela Julia!!"
 
 @app.route("/sobre")
 def sobre():
